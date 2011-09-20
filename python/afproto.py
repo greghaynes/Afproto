9281e2298a3248b21251371df4309581fb298d6a
Gregory Haynes <greg@greghaynes.net>
2011-09-20 04:08:19 -0700

    Added test which inserts garbage chars around msg

--- python/afproto.py
+++ python/afproto.py
@@ -33,15 +33,15 @@ def extract_frame_at(data):
 		return f
 	while not f.is_valid() and end_ndx < len(data):
 		end_ndx = data.find(chr(end_byte), end_ndx+1)
-		f.from_raw(data)
+		f.from_raw(data[:end_ndx+1])
 	return f
 
 def extract_next_frame(data):
 	start_ndx = data.find(chr(start_byte))
 	f = extract_frame_at(data[start_ndx:])
 	while not f.is_valid():
-		f = extract_frame_at(data[start_ndx:])
 		start_ndx = data.find(chr(start_byte), start_ndx+1)
+		f = extract_frame_at(data[start_ndx:])
 	return f
 
 if __name__=='__main__':
@@ -50,6 +50,6 @@ if __name__=='__main__':
 	for b in out:
 		print "%x" % ord(b),
 	print
-	f = extract_next_frame('\xa3\x0a\x48\x65\x6c\x6c\x6f\x00\xc9\x5D')
+	f = extract_next_frame('\x05\x88\x34\xa3\x0a\x48\x65\x6c\x6c\x6f\x00\xc9\x5D\x50\x89')
 	print f.is_valid(), f.payload
 
