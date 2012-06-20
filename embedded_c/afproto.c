6429d04919469e925723d201d2fbee2ba966744c
cosmonaut <guavajellyfish@yahoo.com>
2012-06-20 00:55:28 -0600

    Test serialize and get with several chars that should get escaped

--- embedded_c/afproto.c
+++ embedded_c/afproto.c
@@ -9,6 +9,7 @@
 
 #ifdef TEST_GCC
 #include <stdio.h>
+#include <string.h>
 #endif
 
 uint8_t afproto_get_payload(const unsigned char *buffer,
@@ -96,16 +97,47 @@ int main(int argc, char **argv) {
 	char buff2[256];
 	uint8_t length;
 	int i;
+        uint8_t status = 0;
 
-	length = afproto_serialize_payload("Hello", 5, buff);
+        /* initialize the payload buffer */
+        memset(buff2, 0x00, 256);
+
+        printf("serialize_payload test\n");
+        printf("======================\n");
+	length = afproto_serialize_payload("He\x59\xa3l\xa3l\x85o", 9, buff);
 	for(i = 0;i < length;++i)
 		printf("%x ", (uint8_t)buff[i]);
 	printf("\n");
 
+        printf("\n\nget_payload test 1\n");
+        printf("==================\n");
+        /* Here's a test of the returned length of the packet (should
+         * be 9?) */
+        status = afproto_get_payload(buff, length, buff2, &length);
+        printf("Serialized packet length: %i\n", status);
+        if (length != 9) {
+                printf("Length: %u\n", length);
+        }
+        if (status != 0) {
+                /* in hex... */
+                printf("PAYLOAD LENGTH: %u\n", length);
+                for(i = 0;i < length;i++)
+                        printf("%x ", (uint8_t)buff2[i]);
+                printf("\n");
+                /* in chars ... */
+                for(i = 0;i < length;i++)
+                        printf("%c ", (uint8_t)buff2[i]);
+                printf("\n");
+        }
+
+        printf("\n\nget_payload test 2\n");
+        printf("================\n");
 	afproto_get_payload("\xa3\x0e\x09\x04\x00\x00\x00\x00\x1a\xdf\x85\xa3\xc\x00\x00\x00\x00\x59", 18, buff2, &length);
 	printf("%d: ", length);
 	for(i = 0;i < length;i++)
 		printf("%c", buff2[i]);
 	printf("\n");
+
+        return 0;
 }
 #endif
