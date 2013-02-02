e08a2579221eed6d9938ce30bf803e492a657661
Gregory Haynes <greg@greghaynes.net>
2013-02-01 23:25:06 -0800

    removed embedded c from v2

--- embedded_c/crc8.h
+++
@@ -1,8 +0,0 @@
-#ifndef CRC_8_H
-#define CRC_8_H
-
-unsigned char crc_8_update(unsigned char crc, unsigned char c);
-
-unsigned char crc_8(unsigned const char *pDst, unsigned char length);
-
-#endif
