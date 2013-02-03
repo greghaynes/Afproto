9ec71ee3fb1226acb3cbdbf44fda799b11f73a0a
Gregory Haynes <greg@greghaynes.net>
2013-02-03 13:42:00 -0800

    Afproto C returns / sets discardable bytes on success and failure

--- C/afproto.c
+++ C/afproto.c
@@ -1,27 +1,33 @@
 #include "afproto.h"
+#include "crc16.h"
 
 /*
  * Args:
  *   src      : source raw data
  *   src_len  : length of raw data
  *   dest     : destination buffer, must be at least length of src buffer
- *   dest_len : length of destination buffer
+ *   dest_len : length of data in destination buffer, unless error returned
  * Return values:
  *   -3   : Destination buffer too small
  *   -2   : Invalid CRC
  *   -1   : No valid message found
  *   >= 0 : Message ended at src + return value
+ * If  an error is returned, dest_len is set to the amount of data which can
+ * be (and should be) discarded from the start of the src buffer.
  */
 int afproto_get_data(const char *src,
     unsigned int src_len,
     char *dest,
     unsigned int *dest_len) {
+    const char *src_start = src;
     const char *src_end = src + src_len;
     const char *dest_start = dest;
     short crc_check = 0;
 
     // Advance src to start byte
     while(src < src_end && *src != AFPROTO_START_BYTE) ++src;
+    // Set our error return val for dest_len
+    *dest_len = src - src_start;
     if(src >= src_end)
         return -1;
 
@@ -47,23 +53,20 @@ int afproto_get_data(const char *src,
         ++src;
     }
 
-    // Check CRC
-    // Dummy code
-    short crc = *((short*)src);
-    if(crc != crc_check) {
-        printf("crc is %hx but expected %hx\n", crc, crc_check);
-        return -2;
-    }
-    src += 2;
-
-    // Check end btye
-    if(*src != AFPROTO_END_BYTE) {
-        printf("didnt detect end byte\n");
+    // Check that we actually hit the end
+    if(src[2] != AFPROTO_END_BYTE)
         return -1;
+
+    // Check CRC
+    short crc = *((short*)src);
+    src += 3;
+    if(crc != crc_check) {
+        *dest_len = src - src_start;
+        return -2;
     }
 
     *dest_len = dest - dest_start;
-    return src_end - src;
+    return (src - src_start) - 1;
 }
 
 int afproto_frame_data(const char *src,
@@ -118,11 +121,12 @@ int main(int argc, char **argv) {
         orig_msg[i]=(char)i;
     orig_msg[257] = 0;
 
-    if(afproto_frame_data(orig_msg, 256, buff, &write_len) != 0)
-        printf("Data framing error!\n");
+    int ret;
+    if((ret = afproto_frame_data(orig_msg, 256, buff, &write_len)) < 0)
+        printf("Data framing error %d!\n", ret);
     printf("Frame is %d bytes long\n", write_len);
-    if(afproto_get_data(buff, write_len, buff, &write_len) != 0)
-        printf("Get data error!\n");
+    if((ret = afproto_get_data(buff, write_len, buff, &write_len)) < 0)
+        printf("Get data error %d!\n", ret);
 
     for(i = 0;i < 256;++i) {
         if(buff[i] != orig_msg[i]) {
