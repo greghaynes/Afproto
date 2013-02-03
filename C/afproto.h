cf7d2ad0d42d27f7a93acdcd16902c06bdb13b94
Gregory Haynes <greg@greghaynes.net>
2013-02-03 13:19:06 -0800

    Added function prototypes for C

--- C/afproto.h
+++ C/afproto.h
@@ -5,4 +5,14 @@
 #define AFPROTO_ESC_BYTE 0x7E
 #define AFPROTO_END_BYTE 0x7D
 
+int afproto_get_data(const char *src,
+    unsigned int src_len,
+    char *dest,
+    unsigned int *dest_len);
+
+int afproto_frame_data(const char *src,
+    unsigned int src_len,
+    char *dest,
+    unsigned int *dest_len);
+
 #endif
