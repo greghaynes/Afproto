8ac12af7381018daeb0b9ff4c288e230ae54f810
Gregory Haynes <greg@greghaynes.net>
2013-02-21 23:55:42 -0800

    Added afproto.py for finding start and end of frame in get_data

--- Python/afproto.py
+++ Python/afproto.py
@@ -11,14 +11,38 @@ def afproto_get_data(raw_frame):
        typical usage is to reset a read buffer to this returned data after
        every call
     '''
-    pass
+    start_ndx = 0
+    end_ndx = 0
+    try:
+        prev_escape = False
+        while raw_frame[start_ndx] != start_byte or prev_escape:
+            if prev_escape:
+                prev_escape = False
+            if raw_frame[start_ndx] == esc_byte:
+                prev_escape = True
+            start_ndx += 1
+        end_ndx = start_ndx + 1
+        while raw_frame[end_ndx] != end_byte or prev_escape:
+            if prev_escape:
+                prev_escape = False
+            if raw_frame[end_ndx] == esc_byte:
+                prev_escape = True
+            end_ndx += 1
+    except IndexError:
+        return raw_frame[start_ndx:]
 
 def afproto_frame_data(data):
     '''
        Returns a raw frame which contains the supplied data
     '''
     ret = start_byte
-    ret += data
+    crc = 0
+    for byte in data:
+        if byte in (start_byte, esc_byte):
+            ret += esc_byte
+            crc = crc16.crc16_floating(esc_byte, crc)
+        ret += byte
+        crc = crc16.crc16_floating(byte, crc)
     ret += struct.pack('H', crc16.crc16_buff(data))
     ret += end_byte
     return ret
