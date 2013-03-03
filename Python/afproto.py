e745d4e89159938070019ca27cc316a47741fec5
Gregory Haynes <greg@greghaynes.net>
2013-03-03 11:50:26 -0800

    afproto.py does crc on escaped data

--- Python/afproto.py
+++ Python/afproto.py
@@ -6,9 +6,44 @@ http://github.com/greghaynes/Afproto/tree/v2
 import crc16
 import struct
 
-START_BYTE = chr(0x7D)
-ESC_BYTE = chr(0x7E)
-END_BYTE = chr(0x7D)
+START_BYTE = '\x7D'
+ESC_BYTE = '\x7E'
+END_BYTE = '\x7D'
+
+def unescape_data(data):
+    '''
+    Takes in a string and returns the unescaped data
+    '''
+    ret = ''
+    prev_escape = False
+    for char_ in data:
+        if not prev_escape:
+            if char_ == ESC_BYTE:
+                prev_escape = True
+            else:
+                ret += char_
+        else:
+            ret += chr(ord(char_) ^ 0x20)
+            prev_escape = False
+
+    if prev_escape:
+        raise ValueError('Invalid data, ends in escape byte')
+
+    return ret
+
+
+def escape_data(data):
+    '''
+    Takes in a string and returns an escaped version of the string
+    '''
+    ret = ''
+    for char_ in data:
+        if char_ in (START_BYTE, ESC_BYTE, END_BYTE):
+            ret += 'ESC_BYTE' + chr(ord(char_) ^ 0x20)
+        else:
+            ret += char_
+    return ret
+
 
 def afproto_get_data(raw_frame):
     '''
@@ -51,17 +86,18 @@ def afproto_frame_data(data):
     Returns a raw frame which contains the supplied data
     '''
     ret = START_BYTE
-    crc = 0
-    for byte in data:
-        if byte in (START_BYTE, ESC_BYTE):
-            ret += ESC_BYTE
-            crc = crc16.crc16_floating(ESC_BYTE, crc)
-        ret += byte
-        crc = crc16.crc16_floating(byte, crc)
-    ret += struct.pack('H', crc16.crc16_buff(data))
+    data = escape_data(data)
+    crc = crc16.crc16_buff(data)
+    ret += data
+    ret += struct.pack('H', crc)
     ret += END_BYTE
     return ret
 
+
+def main():
+    for chr_ in afproto_frame_data('test'):
+        print '%x' % ord(chr_),
+
+
 if __name__ == '__main__':
-    for ch in afproto_frame_data('test'):
-        print '%x' % ord(ch),
+    main()
